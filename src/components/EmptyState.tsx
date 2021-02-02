4354ec75a4aa6cad83e7547b837dc3f71a42295b
Nhat Long Nguyen <nhatlongnguyen1992@gmail.com>
2021-02-02 12:39:10 +0100

    Create EmptyState component

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import {Row, Col} from '@zendeskgarden/react-grid'
+import {Button} from '@zendeskgarden/react-buttons'
+
+import EmptyStateSVG from './../svg/EmptyState.svg'
+
+import css from './../App.css'
+
+const EmptyState = () => {
+  return (
+    <Col textAlign="center">
+      <Row justifyContent="center" className={css.contentView}>
+        <EmptyStateSVG width={'60%'} />
+      </Row>
+      <Row justifyContent="center" className={css.contentView}>
+        <Button>Create</Button>
+      </Row>
+    </Col>
+  )
+}
+
+export default EmptyState
