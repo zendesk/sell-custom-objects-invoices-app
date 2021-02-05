7787981262f77d4a375087b6b65fad3bde9e0dbf
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2021-02-05 11:46:36 +0100

    Add moment to display dates

--- src/components/InvoiceView.tsx
+++ src/components/InvoiceView.tsx
@@ -2,59 +2,56 @@ import * as React from 'react'
 import {ResponseHandler, useClientRequest} from '@zendesk/sell-zaf-app-toolbox'
 import {Body, Cell, Row, Table} from '@zendeskgarden/react-tables'
 import {Code, MD} from '@zendeskgarden/react-typography'
+import moment from 'moment'
 
 import Loader from './Loader'
 import EmptyState from './EmptyState'
 import {InvoiceData, InvoiceResponse} from '../providers/sunshineProvider'
 
 const InvoiceItem = ({invoiceData}: {invoiceData: InvoiceData}) => {
-  console.log(invoiceData)
   const {attributes} = invoiceData
 
-  const renderStatus = () =>
-    attributes.is_paid ? (
-      <Code hue="red">Not paid</Code>
-    ) : (
-      <Code hue="green">Paid</Code>
-    )
-
   return (
-    <div style={{overflowX: 'auto'}}>
-      <Table style={{minWidth: 500}}>
-        <Body>
-          <Row>
-            <Cell>
-              <MD isBold>Invoice number</MD>
-            </Cell>
-            <Cell>{attributes.invoice_number}</Cell>
-          </Row>
-          <Row>
-            <Cell>
-              <MD isBold>Issue date</MD>
-            </Cell>
-            <Cell>{attributes.issue_date}</Cell>
-          </Row>
-          <Row>
-            <Cell>
-              <MD isBold>Due date</MD>
-            </Cell>
-            <Cell>{attributes.due_date}</Cell>
-          </Row>
-          <Row>
-            <Cell>
-              <MD isBold>Due amount</MD>
-            </Cell>
-            <Cell>${attributes.due_amount}</Cell>
-          </Row>
-          <Row>
-            <Cell>
-              <MD isBold>Status</MD>
-            </Cell>
-            <Cell>{renderStatus()}</Cell>
-          </Row>
-        </Body>
-      </Table>
-    </div>
+    <Table>
+      <Body>
+        <Row>
+          <Cell>
+            <MD isBold>Invoice number</MD>
+          </Cell>
+          <Cell>{attributes.invoice_number}</Cell>
+        </Row>
+        <Row>
+          <Cell>
+            <MD isBold>Issue date</MD>
+          </Cell>
+          <Cell>{moment(attributes.issue_date).format('ll')}</Cell>
+        </Row>
+        <Row>
+          <Cell>
+            <MD isBold>Due date</MD>
+          </Cell>
+          <Cell>{moment(attributes.due_date).format('ll')}</Cell>
+        </Row>
+        <Row>
+          <Cell>
+            <MD isBold>Due amount</MD>
+          </Cell>
+          <Cell>${attributes.due_amount}</Cell>
+        </Row>
+        <Row>
+          <Cell>
+            <MD isBold>Status</MD>
+          </Cell>
+          <Cell>
+            {attributes.is_paid ? (
+              <Code hue="red">Not paid</Code>
+            ) : (
+              <Code hue="green">Paid</Code>
+            )}
+          </Cell>
+        </Row>
+      </Body>
+    </Table>
   )
 }
 
